e221f73deec322ecaa16bb51e3348d401963efe2
Your Name <you@example.com>
2025-08-14 17:03:19 +0530

    newchange

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+// PostCSS configuration for Tailwind CSS and Autoprefixer.
+//
+// Since this project uses ES modules (type: "module" in package.json), this
+// file needs to use an ES module export instead of CommonJS's `module.exports`.
+// See https://nodejs.org/api/esm.html for details.
+
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
